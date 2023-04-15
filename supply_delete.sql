b4f578f3a2befbedc4768676537f4767bf233d68
indicozy <berkutkarlibai@gmail.com>
2023-04-16 00:29:16 +0600

    unfinishedg

--- supply_delete.sql
+++ supply_delete.sql
@@ -3,7 +3,7 @@ UPDATE sale
   SET margin = margin + (SELECT quantity * price FROM supply WHERE id = <supply_id>) -- idk
   WHERE id IN (SELECT sale_id FROM supply_sale WHERE supply_id = <supply_id>);
 
-DELETE FROM supply_sale WHERE supply_id=<supply_id>
+DELETE FROM supply_sale WHERE supply_id=<supply_id>;
 
 DELETE FROM supply
   WHERE id = <supply_id>;
--- supply_edit.sql
+++ supply_edit.sql
@@ -1,13 +1,13 @@
 -- new_quantity, supply_id
 
 UPDATE sale 
-  SET margin = margin + (SELECT quantity * price FROM supply WHERE id = <supply_id>)
-  WHERE s.id IN (
+  SET margin = margin + (SELECT quantity * price FROM supply WHERE id = supply_id)
+  WHERE id IN (
     SELECT sale_id 
     FROM supply_sale 
-    WHERE supply_id = <supply_id>
+    WHERE supply_id = supply_id
   );
 
 UPDATE supply
-  SET quantity = <new_quantity>
-  WHERE id = <supply_id>;
+  SET quantity = new_quantity
+  WHERE id = supply_id;
